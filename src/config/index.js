7a50f730a72781e4ac7e619fa39ccb3eec9d6ca9
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-06-29 16:06:15 -0300

    ajustes config e db inicial

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
-const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'https://semana1aluraflix.herokuapp.com/videos/';
+// const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
+//   ? 'http://localhost:8080'
+//   : '<url da API após deploy>';
+
+const URL_BACKEND_TOP = 'http://localhost:8080';
 
 export default {
   URL_BACKEND_TOP,
